0dc07efc425f3893029c0131fb51fff9ced65081
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-07 18:45:34 +0300

    upd

--- backend/configs/winston.js
+++ backend/configs/winston.js
@@ -6,7 +6,7 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
     new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: './logs/combined.log' }),
+    new winston.transports.File({ filename: './logs/request.log' }),
   ],
 });
 
