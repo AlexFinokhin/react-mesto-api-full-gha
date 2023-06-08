e38bd0c54d370482e43ebbc765768c4bcd285da5
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-09 00:44:28 +0300

    upd

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -112,8 +112,8 @@ export class Api {
 
 const jwtToken = localStorage.getItem("jwt");
 const api = new Api({
-  baseUrl: "http://localhost:3000",
-  // baseUrl: "https://api.ryangosling.nomoredomains.rocks",
+  // baseUrl: "http://localhost:3000",
+  baseUrl: "https://api.ryangosling.nomoredomains.rocks",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${jwtToken}`,
