2c4705c51e758ed3fb658d08cc0959b104e795f6
Alex Finokhin <101180377+AlexFinokhin@users.noreply.github.com>
2023-06-11 19:25:47 +0300

    Fix: process.env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/401-UnauthorizedError');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 async function verifyToken(request, response, next) {
   const { authorization } = request.headers;
@@ -15,7 +15,7 @@ async function verifyToken(request, response, next) {
   let payload;
 
   try {
-    payload = await jwt.verify(token, process.env.NODE_ENV === 'production' ? JWT_SECRET : 'ryangosling');
+    payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'ryangosling');
   } catch (err) {
     const error = new UnauthorizedError('Для продолжения требуется вход в систему');
     return next(error);
